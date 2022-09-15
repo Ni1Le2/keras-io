b3314ddd61e4d68d1cb09eb7a857f8f6d9e136da
Ni1Le2 <67645591+Ni1Le2@users.noreply.github.com>
2022-09-15 18:25:24 +0200

    Create create_image_mask.py

---
+++ code/create_image_mask.py
@@ -0,0 +1,8 @@
+# https://www.digitalocean.com/community/tutorials/arithmetic-bitwise-and-masking-python-opencv
+
+import numpy as np
+import cv2
+
+image_path = ""
+image = cv2.imread(image_path)
+imshow(image)
